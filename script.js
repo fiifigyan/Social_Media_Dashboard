9181e0f8b1e8400bead1bd79236e57dbc90e340c
Christian Gyan <chrisgyan33@gmail.com>
2023-11-21 13:31:55 +0000

    CSS Modifications

--- script.js
+++ script.js
@@ -31,35 +31,35 @@ if (lightMode === 'light-mode') {
 lightModeToggle.addEventListener('click', () => {
   if(image.src.endsWith(darkBtn)){
             image.src = lightBtn;
-       }else{
+  }
+  else{
             image.src = darkBtn;
-       }
+      }
 
     body.classList.toggle('light');
        
     if(image.src.endsWith(lightBtn)){
       localStorage.setItem('light', 'light-mode');
-    }else if(image.src.endsWith(darkBtn)){
+    }
+    else if(image.src.endsWith(darkBtn)){
       localStorage.setItem('light', 'dark-mode');
     }
-
 });
-
-
 
 const fetchData = (url, callback) =>{
   // Create a new XMLHttpRequest object
   const http = new XMLHttpRequest();
-   // Open a GET request to the specified URL
+  // Open a GET request to the specified URL
   http.open('get', url, true);
   // Define an event handler for when the request state changes
   http.onreadystatechange = function(){
-    // Check if the request is complete (readyState 4) and if the status is OK (HTTP status 200)
+    // Check if the request is complete (readyState 4 means request is finished and response is ready) 
+    //and if the status is OK (HTTP status 200 means request is "OK")
     if(this.readyState == 4 && http.status == 200){
       // Parse the response text as JSON
-       const data = JSON.parse(http.responseText);
-       // Call the callback function with the parsed data as an argument
-       callback(data); 
+      const data = JSON.parse(http.responseText);
+      // Call the callback function with the parsed data as an argument
+      callback(data); 
     }
   }
   // Send the HTTP request
@@ -74,7 +74,7 @@ document.addEventListener('DOMContentLoaded', () =>{
     let sectionHTML = "";
     mediaContainer.forEach((banner) => {
       sectionHTML +=`
-        <div class = "media-banner" id="${banner["media_id"]}">
+        <div class = "media-banner" id="${banner["media_id"]}" style="border-top:${banner["border_color"]}">
           <div class="media-icon">
             <img src="${banner["media_logo"]}">
             <p id="username">${banner["username"]}</p>
@@ -92,8 +92,6 @@ document.addEventListener('DOMContentLoaded', () =>{
     });
     document.querySelector('.media-container').innerHTML = sectionHTML;
 
-
-
     let overviewHTML = "";
     overviewContainer.forEach((card) => {
       overviewHTML +=`
@@ -102,6 +100,7 @@ document.addEventListener('DOMContentLoaded', () =>{
             <h3>${card["metric"]}</h3>
             <img src="${card["media_logo"]}">
         </div>
+        
         <div id="countnumber">
             <p id="countdigit">${card["count_digit"]}</p>
             <div id="countup">
